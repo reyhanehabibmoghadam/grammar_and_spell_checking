b8814dd054accee03f4b7eac1974c33b38bbe7a6
reyhanehabibmoghadam <139012548+reyhanehabibmoghadam@users.noreply.github.com>
2023-07-08 23:49:47 +0330

    Add files via upload

--- grammar_and_spell_checking.py
+++ grammar_and_spell_checking.py
@@ -1,18 +1,24 @@
 from spellchecker import SpellChecker
 
-spell = SpellChecker()
+def grammar_check(text):
+    spell = SpellChecker()
+    words = text.split()
+    corrected_words = []
+    for word in words:
+        corrected_word = spell.correction(word)
+        if corrected_word is not None:
+            corrected_words.append(corrected_word)
+        else:
+            corrected_words.append(word)  # Use the original word if no correction found
+
+    corrected_text = ' '.join(corrected_words)
+
+    return corrected_text
+
+def simplify_grammar(text):
+    corrected_text = grammar_check(text)
+    return corrected_text
 
 input_text = "I wnt too tha store to by som fruts and vgitables."
-
-words = input_text.split()
-corrected_words = []
-for word in words:
-    corrected_word = spell.correction(word)
-    if corrected_word is not None:
-        corrected_words.append(corrected_word)
-    else:
-        corrected_words.append(word)
-
-corrected_text = ' '.join(corrected_words)
-
+corrected_text = simplify_grammar(input_text)
 print(corrected_text)
